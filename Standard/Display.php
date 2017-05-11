395ad67ba440302c3626b0677ff3b24ad8adc4b6
操戈 <caogemail@163.com>
2017-05-11 23:07:27 +0800

    启动帮助

--- Standard/Display.php
+++ Standard/Display.php
@@ -8,11 +8,10 @@
 
 namespace Showin\Standard;
 
-
 class Display
 {
-    public static function component()
+    public static function help()
     {
-        Output::stdout(Output::COLOR_RED . "\n启动参数错误\n    message \n\n启动参数错误\n    message\n" . Output::COLOR_NONE);
+        Output::stdout(Output::COLOR_GREEN . "\n-c 启动组件\n   Container:容器\n   Proxy:代理\n   Registry:注册中心\n\n-h 监听host\n   默认:0.0.0.0\n\n-p 监听端口\n   默认:9500,9501,9502\n" . Output::COLOR_NONE);
     }
 }
--- Start.php
+++ Start.php
@@ -5,7 +5,7 @@
  * Date: 2017/4/18
  * Time: 下午9:39
  */
-use Showin\Standard\Output;
+use Showin\Standard\Display;
 use Showin\Psr\Psr4AutoloaderClass;
 use Showin\Component\Container\ContainerServer;
 use Showin\Component\Proxy\ProxyServer;
@@ -32,7 +32,7 @@ switch ($component) {
         $server = new RegistryServer($host, $port);
         break;
     default:
-        Output::stdout(Output::COLOR_GREEN . "\n-c 启动组件\n   Container:容器\n   Proxy:代理\n   Registry:注册中心\n\n-h 监听host\n   默认:0.0.0.0\n\n-p 监听端口\n   默认:9500,9501,9502\n" . Output::COLOR_NONE);
+        Display::help();
         exit;
         break;
 }
